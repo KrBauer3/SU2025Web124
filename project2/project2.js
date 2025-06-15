ac8d60513837f5fc7d7c244bf2ea0b164017d26d
KrBauer3 <bauerjkristen@gmail.com>
2025-06-15 15:32:39 -0500

    Add files via upload
    project 2 complete

---
+++ project2/project2.js
@@ -0,0 +1,40 @@
+// JavaScript source code
+
+//Kristen Bauer 7-25-2024
+
+"use strict"; //"use strict";
+
+var myName;
+myName = "Kristen Bauer";
+var para1 = document.getElementById("p1");
+para1.textContent = myName;
+
+var n1 = 5;
+var n2 = 10;
+var numberSum = n1 + n2;
+var para2 = document.getElementById("p2");
+para2.textContent = numberSum;
+
+var numberMult = n1 * n2;
+var para3 = document.getElementById("p3");
+para3.textContent = numberMult;
+
+var myNameAddNum = n1 + myName;
+var para4 = document.getElementById("p4");
+para4.textContent = myNameAddNum;
+
+var myNameMultNum = n2 * myName;
+var para5 = document.getElementById("p5");
+para5.textContent = myNameMultNum;
+
+var age1 = 19;
+var age2 = 25;
+var ageCompare = age1 > age2;
+var para6 = document.getElementById("p6");
+para6.textContent = ageCompare;
+
+
+
+
+
+
