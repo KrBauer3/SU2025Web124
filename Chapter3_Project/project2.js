a479ae2ac1c429241a10d81e758ff4fd9f183762
KrBauer3 <bauerjkristen@gmail.com>
2025-06-28 16:51:51 -0500

    Add files via upload

---
+++ Chapter3_Project/project2.js
@@ -0,0 +1,42 @@
+// JavaScript source code
+
+//Kristen Bauer 6-28-2025
+
+"use strict";
+
+var myName;
+myName = "Kristen Bauer";
+//var para1 = document.getElementById("p1");
+//para1.textContent = myName;
+console.log(myName);
+
+var dreamSalary;
+dreamSalary = 200000; //go big or go home am I right? 
+console.log(dreamSalary);
+
+var vetStat = false;
+console.log(vetStat);
+
+var friends = [Laz, Snarky, Juice]; //Gamer names amIright
+console.log(friends);
+
+var frienSalary = [85000, 95000, 100000]; //"modest"
+console.log(frienSalary);
+
+var anotherOne = {
+  nameFirst: Calvin,
+  nameLast: Nivlac, //Backwards
+  Sal: 130000, //How did he manage that, huh? Give it to me. 
+}
+
+
+
+
+
+
+
+
+
+
+
+
